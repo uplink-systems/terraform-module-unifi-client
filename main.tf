324ca58e4d2ab90d311d0ff02a28ee29268ac894
Andreas Schreiner <andreas.schreiner@uplink.systems>
2025-12-09 13:46:06 +0100

    fixed network_id rule

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@
 resource "unifi_user" "user" {
     mac                     = var.client.mac
     name                    = var.client.name
-    network_id              = data.unifi_network.network.id
+    network_id              = var.client.network == null ? null : data.unifi_network.network.id
     site                    = var.client.site == null ? "default" : var.client.site
     allow_existing          = var.client.user.allow_existing == null ? true : var.client.user.allow_existing
     blocked                 = var.client.user.blocked == null ? false : var.client.user.blocked
@@ -14,7 +14,7 @@ resource "unifi_user" "user" {
     local_dns_record        = var.client.user.fixed_ip == null ? null : var.client.user.local_dns_record
     note                    = var.client.user.note
     skip_forget_on_destroy  = var.client.user.skip_forget_on_destroy == null ? false : var.client.user.skip_forget_on_destroy
-    user_group_id           = data.unifi_user_group.user_group.id
+    user_group_id           = var.client.user.user_group == null ? null : data.unifi_user_group.user_group.id
 }
 
 resource "unifi_account" "account" {
